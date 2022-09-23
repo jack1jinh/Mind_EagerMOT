eac675ac51e92cc00c04d2e9810cb4b4c4403fbe
jack1jinh <62103150+jack1jinh@users.noreply.github.com>
2022-09-23 13:47:42 +0800

    Add files via upload

---
+++ config/local_variables.py
@@ -0,0 +1,10 @@
+MOUNT_PATH = "/data0/JIN/ms_EagerMOT"  # in case you are mounting data storage externally
+SPLIT = 'training'
+
+#KITTI_WORK_DIR = MOUNT_PATH + "/storage/slurm/kimal/eagermot_workspace/kitti"
+KITTI_WORK_DIR = MOUNT_PATH + "/storage copy/workspace/kitti"
+#KITTI_DATA_DIR = MOUNT_PATH + "/storage/slurm/osep/datasets/kitti"
+KITTI_DATA_DIR = MOUNT_PATH + "/storage copy/datasets/kitti"
+
+NUSCENES_WORK_DIR = MOUNT_PATH + "/storage/slurm/kimal/eagermot_workspace/nuscenes"
+NUSCENES_DATA_DIR = MOUNT_PATH + "/storage/slurm/kimal/datasets_original/nuscenes"
